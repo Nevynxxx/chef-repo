655fb751f5a61a8b622c2624aa045e96b5265bb8
Anthony Metcalf <anthony.metcalf@alphahospitals.co.uk>
2015-05-20 11:20:08 +0100

    Latest version from github

--- cookbooks/database/libraries/resource_postgresql_database.rb
+++ cookbooks/database/libraries/resource_postgresql_database.rb
@@ -1,15 +1,15 @@
 #
-# Author:: Seth Chisamore (<schisamo@opscode.com>)
-# Author:: Lamont Granquist (<lamont@opscode.com>)
-# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# Author:: Seth Chisamore (<schisamo@chef.io>)
+# Author:: Lamont Granquist (<lamont@chef.io>)
+# Copyright:: Copyright (c) 2011 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,13 +23,11 @@ require File.join(File.dirname(__FILE__), 'provider_database_postgresql')
 class Chef
   class Resource
     class PostgresqlDatabase < Chef::Resource::Database
-
-      def initialize(name, run_context=nil)
+      def initialize(name, run_context = nil)
         super
         @resource_name = :postgresql_database
         @provider = Chef::Provider::Database::Postgresql
       end
-
     end
   end
 end
--- cookbooks/database/recipes/postgresql.rb
+++ cookbooks/database/recipes/postgresql.rb
@@ -1,15 +1,14 @@
 #
-# Author:: Joshua Timberman (<joshua@opscode.com>)
-# Cookbook Name:: database
-# Recipe:: default
+# Author:: Jesse Howarth (<him@jessehowarth.com>)
 #
-# Copyright 2009, Opscode, Inc.
+# Copyright:: Copyright (c) 2012, Chef Software, Inc. (<legal@chef.io>)
+# License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,3 +17,4 @@
 # limitations under the License.
 #
 
+include_recipe 'postgresql::ruby'
